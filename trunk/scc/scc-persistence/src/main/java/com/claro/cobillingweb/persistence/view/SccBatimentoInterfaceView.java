69e886ac9a7f76d684fda3bede8cb9bfc64623c3
1lutador@gmail.com <1lutador@gmail.com@54131ace-94e0-7b28-e8c0-f2ad73b6ed4e>
2012-09-26 20:04:54 +0000

    RF06 - POS PAGO

---
+++ trunk/scc/scc-persistence/src/main/java/com/claro/cobillingweb/persistence/view/SccBatimentoInterfaceView.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package com.claro.cobillingweb.persistence.view;
+
+import java.util.Date;
+
+/**
+ * @author 93046251
+ *
+ */
+public class SccBatimentoInterfaceView {
+	
+	private String nomeArquivo;
+	private Long operadoraLD;
+	private Long operadoraClaro;
+	private Date dataMovimentacao;
+	private Date dataTransferencia;
+	private Long quantidadeRegistrosMobile;
+	private Date dataProcessamento;
+	private Long quantidadeRegistrosScc;
+	
+	
+	public String getNomeArquivo() {
+		return nomeArquivo;
+	}
+	public void setNomeArquivo(String nomeArquivo) {
+		this.nomeArquivo = nomeArquivo;
+	}
+	public Long getOperadoraLD() {
+		return operadoraLD;
+	}
+	public void setOperadoraLD(Long operadoraLD) {
+		this.operadoraLD = operadoraLD;
+	}
+	public Long getOperadoraClaro() {
+		return operadoraClaro;
+	}
+	public void setOperadoraClaro(Long operadoraClaro) {
+		this.operadoraClaro = operadoraClaro;
+	}
+	public Date getDataMovimentacao() {
+		return dataMovimentacao;
+	}
+	public void setDataMovimentacao(Date dataMovimentacao) {
+		this.dataMovimentacao = dataMovimentacao;
+	}
+	public Date getDataTransferencia() {
+		return dataTransferencia;
+	}
+	public void setDataTransferencia(Date dataTransferencia) {
+		this.dataTransferencia = dataTransferencia;
+	}
+	public Long getQuantidadeRegistrosMobile() {
+		return quantidadeRegistrosMobile;
+	}
+	public void setQuantidadeRegistrosMobile(Long quantidadeRegistrosMobile) {
+		this.quantidadeRegistrosMobile = quantidadeRegistrosMobile;
+	}
+	public Date getDataProcessamento() {
+		return dataProcessamento;
+	}
+	public void setDataProcessamento(Date dataProcessamento) {
+		this.dataProcessamento = dataProcessamento;
+	}
+	public Long getQuantidadeRegistrosScc() {
+		return quantidadeRegistrosScc;
+	}
+	public void setQuantidadeRegistrosScc(Long quantidadeRegistrosScc) {
+		this.quantidadeRegistrosScc = quantidadeRegistrosScc;
+	}	
+	
+	
+
+}
