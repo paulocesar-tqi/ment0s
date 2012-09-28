41c784f2cfd3f3e5e489ad9868b83dd6cd482d13
1lutador@gmail.com <1lutador@gmail.com@54131ace-94e0-7b28-e8c0-f2ad73b6ed4e>
2012-09-28 19:57:18 +0000

    RF06 - POS PAGO

--- scc/scc-web/src/main/java/com/claro/sccweb/controller/arquivo/PesquisaBatimentoInterfacePosExcelHandler.java
+++ scc/scc-web/src/main/java/com/claro/sccweb/controller/arquivo/PesquisaBatimentoInterfacePosExcelHandler.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 
 import com.claro.sccweb.controller.BaseFormController;
 import com.claro.sccweb.controller.BasicExcelHandler;
@@ -16,6 +19,7 @@ import com.claro.sccweb.controller.ControllerExecutionException;
 import com.claro.sccweb.decorator.rownum.entity.SccBatimentoInterfaceViewDecorator;
 import com.claro.sccweb.excel.ExcelColumnDefinition;
 import com.claro.sccweb.excel.ExcelPrinter;
+import com.claro.sccweb.excel.style.ExcelStyle;
 
 public class PesquisaBatimentoInterfacePosExcelHandler extends BasicExcelHandler {
 
@@ -29,7 +33,7 @@ public class PesquisaBatimentoInterfacePosExcelHandler extends BasicExcelHandler
 			throw new ControllerExecutionException("Navegação inválida. Tabela é nula!.");
 		}
 		List<ExcelColumnDefinition> columnDefinitions = new ArrayList<ExcelColumnDefinition>();
-		columnDefinitions.add(new ExcelColumnDefinition("getNomeArquivo",	"Nome do Arquivo", style, 60));
+		columnDefinitions.add(new ExcelColumnDefinition("getNomeArquivo",	"Nome do Arquivo", style, 40));
 		columnDefinitions.add(new ExcelColumnDefinition("getOperadoraLD",	"Operadora LD", style, 15));
 		columnDefinitions.add(new ExcelColumnDefinition("getOperadoraClaro", "Operadora Claro", style, 15));
 		columnDefinitions.add(new ExcelColumnDefinition("getDataMovimentacao",	"Data de Movimentação", style, 20));
@@ -38,7 +42,7 @@ public class PesquisaBatimentoInterfacePosExcelHandler extends BasicExcelHandler
 		columnDefinitions.add(new ExcelColumnDefinition("getDataProcessamento",	"Data de Processamento", style, 20));
 		columnDefinitions.add(new ExcelColumnDefinition("getQuantidadeRegistrosScc",	"Quantidade de Registros", style, 20));
 		columnDefinitions.add(new ExcelColumnDefinition("getDiferenca",	"Diferença", style, 10));
-		columnDefinitions.add(new ExcelColumnDefinition("getStatus",	"Status", style, 10));
+		columnDefinitions.add(new ExcelColumnDefinition("getStatus",	"Status", style, 8));
 		ExcelPrinter printer = new ExcelPrinter(columnDefinitions,workbook);
 		printer.addSheet("Batimento Interface Pos");
 		List<String> linhasCabecalho = new ArrayList<String>();
@@ -48,6 +52,56 @@ public class PesquisaBatimentoInterfacePosExcelHandler extends BasicExcelHandler
 		printer.setHeaderLines(linhasCabecalho);
 		printer.generateHeader();
 		printer.addBlankLines(1);
+		
+		List<String> linha = new ArrayList<String>();
+		linha.add(" ");
+		linha.add(" ");
+		linha.add(" ");
+		linha.add("MOBILE");
+		linha.add(" ");
+		linha.add(" ");
+		linha.add("SCC");
+		linha.add(" ");
+		linha.add(" ");
+		linha.add(" ");
+		List<Short> mergeRanges = new ArrayList<Short>();
+		mergeRanges.add((short) 3);
+		mergeRanges.add((short) 5);
+		mergeRanges.add((short) 6);
+		mergeRanges.add((short) 7);
+		ExcelStyle myStyle = new ExcelStyle() {
+			
+			public String getFontName() {
+				return "Arial";
+			}
+			 
+			public short getFontColor() {
+				return HSSFColor.BLUE.index;
+			}
+
+			public short getFontHeight() {
+				return 8;
+			}
+
+			public short getBoldweight() {
+				return HSSFFont.BOLDWEIGHT_BOLD;
+			}
+
+			public short getAlignment() {
+				return HSSFCellStyle.ALIGN_CENTER;
+			}
+
+			public boolean getWrapText() {
+				return true;
+			}
+
+			public String getFormat() {		
+				return null;
+			}
+
+		};
+		
+		printer.addLine(linha, mergeRanges, myStyle);
 		printer.generateColumnsTitle();
 		printer.addData(decoratorList);
 		printer.writeData();
